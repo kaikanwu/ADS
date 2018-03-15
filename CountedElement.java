c874bbe28ec1302c891a0455750767f321c890c4
kaikanwu <wukaikan@gmail.com>
2018-03-15 23:11:35 +0000

    part A

--- CountedElement.java
+++ CountedElement.java
@@ -4,20 +4,46 @@ public class CountedElement<E extends Comparable<E>> implements Comparable<Count
 
 	public CountedElement(E e, int count){
 		//constructor - to complete
+		this.element = e;
+		this.count = count;
 	}
 	
 	public CountedElement(E e){
 		//constructor - to complete
+		this.element = e;
 	}
 
 	//add getters and setters
-	
-	//add toString() method
-	
-	
 
-	
-	public int compareTo(CountedElement<E> sC1) {
+	public E getElement() {
+		return element;
+	}
+
+	public void setElement(E element) {
+		this.element = element;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+	//add toString() method
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "CountedElement{" +
+                "element=" + element +
+                ", count=" + count +
+                '}';
+    }
+
+    public int compareTo(CountedElement<E> sC1) {
     
 		//to complete
 	}
